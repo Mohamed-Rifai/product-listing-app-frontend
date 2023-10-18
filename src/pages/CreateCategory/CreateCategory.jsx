5087425882dd52ced406c6bf16a89a9971f81312
Mohamed-Rifai <rifaeeckm@gmail.com>
2023-10-18 08:48:47 +0530

    design update

--- src/pages/CreateCategory/CreateCategory.jsx
+++ src/pages/CreateCategory/CreateCategory.jsx
@@ -1,15 +1,114 @@
+// import React, { useEffect, useState } from "react";
+// import { Modal, Button } from 'react-bootstrap';
+// import axios from "../../axios";
+// import { Link } from "react-router-dom";
+
+// const CreateCategory = () => {
+
+
+//   return (
+//     <div className="container mt-4">
+//       <h2>Add Your Categories</h2>
+     
+//       <div className="row">
+//         <form onSubmit={handleSubmit} className="row">
+//           <div className="col-8">
+//             <input
+//               type="text"
+//               name="name"
+//               className="form-control"
+//               placeholder="Category Name"
+//               value={categoryName}
+//               required
+//               onChange={handleCategoryName}
+//             />
+//           </div>
+//           <div className="col-4">
+//             <button className="btn btn-primary" type="submit">
+//               Add Category
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//       <div className="mt-4">
+//         <h4>Main Categories</h4>
+//         <ul>
+//           {mainCategories.map((category, index) => (
+//             <li key={index}>
+//               <div className="d-flex align-items-center p-3 ">
+//                 <h5>{category.name}</h5>
+//                 <button
+//                   className="btn btn-primary p-1  "
+//                   onClick={() => openModal(category._id)}
+//                 >
+//                   Add Subcategory
+//                 </button>
+//               </div>
+//               <ul>
+
+//                 {/* Render subcategories  */}
+//                 {subCategories
+//                   .filter(subcategory => subcategory.parent === category._id)
+//                   .map((subcategory, subIndex) => (
+//                     <li key={subIndex}>{subcategory.name}</li>
+//                   ))}
+//               </ul>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       <Link to="/">Home</Link> 
+
+//       <Modal show={showModal} onHide={() => setShowModal(false)}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>Add Subcategory</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <input
+//             type="text"
+//             name="subCategoryName"
+//             className="form-control"
+//             placeholder="Subcategory Name"
+//             value={subCategoryName}
+//             required
+//             onChange={handleSubCategoryName}
+//           />
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={() => setShowModal(false)}>
+//             Close
+//           </Button>
+//           <Button variant="primary" onClick={handleAddSubcategory}>
+//             Add Subcategory
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </div>
+//   );
+// };
+
+// export default CreateCategory;
+
+
+
+
+
 import React, { useEffect, useState } from "react";
 import { Modal, Button } from 'react-bootstrap';
 import axios from "../../axios";
 import { Link } from "react-router-dom";
 
-const CreateCategory = () => {
-  const [mainCategories, setMainCategories] = useState([]);
-  const [subCategories, setSubCategories] = useState([]);
-  const [categoryName, setCategoryName] = useState("");
-  const [subCategoryName, setSubCategoryName] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [categoryId, setCategoryId] = useState('');
+
+
+   const CreateCategory = () => { 
+
+     const [mainCategories, setMainCategories] = useState([]);
+     const [subCategories, setSubCategories] = useState([]);
+     const [categoryName, setCategoryName] = useState("");
+     const [subCategoryName, setSubCategoryName] = useState("");
+     const [showModal, setShowModal] = useState(false);
+     const [categoryId, setCategoryId] = useState('');
+     
 
   useEffect(() => {
     // Fetch all categories
@@ -42,6 +141,8 @@ const CreateCategory = () => {
     axios.post("/add-category", { categoryName })
       .then((res) => {
         console.log("category added", res.data);
+       
+        window.location.reload();
       })
       .catch((err) => {
         console.log("category adding error", err);
@@ -49,26 +150,33 @@ const CreateCategory = () => {
   };
 
   const handleAddSubcategory = () => {
-    // send subcategory name and parent category ID to backend
-    axios.post("/add-subcategory", { subCategoryName, categoryId })
-      .then((res) => {
-        console.log("subcategory added", res.data);
-        setShowModal(false);
-      })
-      .catch((err) => {
-        console.log("subcategory adding error", err);
-      });
+    if (subCategoryName) {
+      // send subcategory name and parent category ID to the backend
+      axios
+        .post("/add-subcategory", { subCategoryName, categoryId })
+        .then((res) => {
+          console.log("subcategory added", res.data);
+          setShowModal(false);
+          window.location.reload()
+        })
+        .catch((err) => {
+          console.log("subcategory adding error", err);
+        });
+    } else {
+      alert('Field is required');
+    }
   };
+  
 
   const openModal = (id) => {
     setShowModal(true);
     setCategoryId(id);
   };
 
+
   return (
     <div className="container mt-4">
       <h2>Add Your Categories</h2>
-     
       <div className="row">
         <form onSubmit={handleSubmit} className="row">
           <div className="col-8">
@@ -94,18 +202,16 @@ const CreateCategory = () => {
         <ul>
           {mainCategories.map((category, index) => (
             <li key={index}>
-              <div className="d-flex align-items-center p-3 ">
+              <div className="d-flex justify-content-between align-items-center p-3 border-bottom">
                 <h5>{category.name}</h5>
                 <button
-                  className="btn btn-primary p-1  "
+                  className="btn btn-primary"
                   onClick={() => openModal(category._id)}
                 >
                   Add Subcategory
                 </button>
               </div>
               <ul>
-
-                {/* Render subcategories  */}
                 {subCategories
                   .filter(subcategory => subcategory.parent === category._id)
                   .map((subcategory, subIndex) => (
@@ -116,7 +222,7 @@ const CreateCategory = () => {
           ))}
         </ul>
       </div>
-      <Link to="/">Home</Link> 
+      <Link to="/">Home</Link>
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
@@ -147,5 +253,6 @@ const CreateCategory = () => {
 };
 
 export default CreateCategory;
+
 
 
--- src/pages/ProductLists/ProductList.jsx
+++ src/pages/ProductLists/ProductList.jsx
@@ -1,47 +1,41 @@
 import React, { useState, useEffect } from "react";
-import axios from "../../axios";
 import { Link } from "react-router-dom";
+import axios from "../../axios";
+import './productList.css'
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  const [categories, setCategories] = useState([])
-  const [subcategories,setSubCatagory] = useState([])
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const [selectedSubCategory, setSelectedSubCategory] = useState('');
+  const [selectedSubCategory, setSelectedSubCategory] = useState("");
 
-
-
-
- 
   useEffect(() => {
     // Get the products from the backend
-    axios.get("/get-all-products")
+    axios
+      .get("/get-all-products")
       .then(function (res) {
-      
         setProducts(res.data.products);
-        setCategories(res.data.mainCategories)
-        setSubCatagory(res.data.subcategories)
+        setCategories(res.data.mainCategories);
+        setSubcategories(res.data.subcategories);
       })
       .catch(function (err) {
         console.log(err);
       });
   }, []);
 
-
   const filterProducts = (products, category) => {
     if (category === "All") {
       return products;
     } else {
-     
       return products.filter((product) => product.category.parent === category._id);
     }
   };
-  
-  const filteredProducts = filterProducts(products, selectedCategory);
-  
 
-   // Filter subcategories of main category
-   const filteredSubcategories = subcategories.filter(
+  const filteredProducts = filterProducts(products, selectedCategory);
+
+  // Filter subcategories of main category
+  const filteredSubcategories = subcategories.filter(
     (subcategory) => subcategory.parent === selectedCategory._id
   );
 
@@ -50,98 +44,74 @@ const ProductList = () => {
     (product) => product.category.name === selectedSubCategory
   );
 
-  const totalCount = products.length
- 
-  
   return (
     <div className="container">
       <header className="mb-4">
         <h1 className="text-center">Enjoy our products!</h1>
       </header>
 
-      <h2 className="text-center mb-3">Electronics ({products.length})</h2>
-      <div className="d-flex justify-content-end">
-        <Link to="/add-products" className="btn btn-success">
-          Add Product
-        </Link>
-        <Link to="/create-category" className="btn btn-info">
-          Add Category
-        </Link>
+      <div className="d-flex justify-content-between">
+        <h2 className="mb-3">Electronics ({products.length})</h2>
+        <div className="btn-group">
+          <Link to="/add-products" className="btn btn-success">
+            Add Product
+          </Link>
+          <Link to="/create-category" className="btn btn-info">
+            Add Category
+          </Link>
+        </div>
       </div>
-      <p className="mb-2" onClick={() => setSelectedCategory("All")}>Subcategories: </p>
-     
-      <ul className="list-group" >
-      <li className="list-group-item">
-     
-   </li>
-  {categories.map((category) => (
-    
-    <li className="list-group-item" key={category.id}>
-      <Link  
-      onClick={() => {
-        setSelectedCategory(category); 
-          
-        }}>
-        {category.name} ({category.productCount})
-      </Link>
-    </li>
-  ))}
-      </ul>
 
-
+      <div className="mb-2">
+        <p className="mb-0" onClick={() => setSelectedCategory("All")}>
+          Subcategories:
+        </p>
+        <ul className="list-group">
+          {categories.map((category) => (
+            <li className="list-group-item" key={category.id}>
+              <Link onClick={() => setSelectedCategory(category)}>
+                {category.name} ({category.productCount})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       {filteredSubcategories.length > 0 && (
-        <>
-          <p className="mb-2">Subcategories:</p>
+        <div className="mb-2">
+          <p>Subcategories:</p>
           <ul className="list-group" style={{ display: "flex", flexDirection: "column" }}>
             {filteredSubcategories.map((subcategory) => (
-            <li>  <Link className="list-group-item" key={subcategory._id} onClick={() => setSelectedSubCategory(subcategory.name)}>
-                {subcategory.name} ({subcategory.productCount})
-              </Link></li>
+              <li key={subcategory._id}>
+                <Link className="list-group-item" onClick={() => setSelectedSubCategory(subcategory.name)}>
+                  {subcategory.name} ({subcategory.productCount})
+                </Link>
+              </li>
             ))}
           </ul>
-        </>
+        </div>
       )}
 
       <div className="row mt-5">
-        {selectedSubCategory
-          ? filteredProductsBySubcategory.map((product) => (
-              <div className="col-md-4 mb-3" key={product._id}>
-                <div className="card">
-                  <img className="card-img-top" src={'https://via.placeholder.com/200x200'} alt={product.name} />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.price}</p>
-                    <Link to={`/products/${product._id}`} className="btn btn-primary">
-                      View product
-                    </Link>
-                  </div>
-                </div>
+        {(selectedSubCategory ? filteredProductsBySubcategory : filteredProducts).map((product) => (
+          <div className="col-md-3 mb-3" key={product._id}>
+            <div className="card">
+              <img className="card-img-top" src={ 'https://via.placeholder.com/200x200'} alt={product.name} />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">$: {product.price}</p>
+                <Link to={`/products/${product._id}`} className="btn btn-primary">
+                  View product
+                </Link>
               </div>
-            ))
-          : filteredProducts.map((product) => (
-              <div className="col-md-4 mb-3" key={product._id}>
-                <div className="card">
-                  <img className="card-img-top" src={product.image} alt={product.name} />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.price}</p>
-                    <Link to={`/products/${product._id}`} className="btn btn-primary">
-                      View product
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
+            </div>
+          </div>
+        ))}
       </div>
-
-
     </div>
   );
 };
 
 export default ProductList;
-
-
 
 
